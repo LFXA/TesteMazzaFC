7bd1dde37baedace4d8bd05d59193edc4d777075
LucasFelixAquino <40723340+LucasFelixAquino@users.noreply.github.com>
2021-06-07 17:16:37 -0300

    first commit

---
+++ TesteMazzaFC/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteMazzaFC.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [Required]
+        [EmailAddress]
+        public string txtEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string txtPassword { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+             
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}

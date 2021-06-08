19869812ea68f80310e2f2a0b0608a93f093d964
LucasFelixAquino <40723340+LucasFelixAquino@users.noreply.github.com>
2021-06-08 01:22:49 -0300

    conexão com api

--- TesteMazzaAPI/Controllers/AuthController.cs
+++ TesteMazzaAPI/Controllers/AuthController.cs
@@ -3,17 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TesteMazzaAPI.Models;
 
 namespace TesteMazzaAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AuthController : Controller
+    public class AuthController : ControllerBase
     {
-       
-        public IActionResult Index()
+       [HttpPost("login")]
+        public IActionResult Index([FromForm]Login login)
         {
-            return View();
+            return Ok(new { token = login.Email });
         }
     }
 }
--- TesteMazzaFC/Pages/Index.cshtml.cs
+++ TesteMazzaFC/Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace TesteMazzaFC.Pages
@@ -13,10 +17,12 @@ namespace TesteMazzaFC.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty]
         [Required]
         [EmailAddress]
         public string txtEmail { get; set; }
 
+        [BindProperty]
         [Required]
         [DataType(DataType.Password)]
         public string txtPassword { get; set; }
@@ -31,15 +37,46 @@ namespace TesteMazzaFC.Pages
         }
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Produtos");
 
             if (ModelState.IsValid)
             {
-             
-            }
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44326/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    var formContent = new FormUrlEncodedContent(new[]
+                    {
+                       new KeyValuePair<string, String>("Email", txtEmail),
+                       new KeyValuePair<string, String>("Password", txtPassword)
 
-            // If we got this far, something failed, redisplay form
-            return Page();
-        }
-    }
+                   });
+                    var body = JsonConvert.SerializeObject(new { Email = txtEmail, Password = txtPassword });
+
+                    var result = await client.PostAsync("/api/auth/login", formContent);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var response = await result.Content.ReadAsStringAsync();
+                        var jObject = JObject.Parse(response);
+                        var token = jObject.GetValue("token").ToString();
+                        
+                        HttpContext.Session.Set("token", Encoding.ASCII.GetBytes(token));
+
+                        _logger.LogInformation("User logado.");
+                        return LocalRedirect(returnUrl);
+                    }
+
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Login Inválido.");
+                        return Page();
+                    }
+                }
+
+                // If we got this far, something failed, redisplay form
+            } 
+             return Page();
+        } 
+    }        
 }
